a6422b739fc470a7f3c84ab271f417f5f18d7b26
liuyuqing <2273852279@qq.com>
2019-12-07 12:06:12 +0800

    sql 修改

--- src/main/resources/amir数据库.sql
+++ src/main/resources/amir数据库.sql
@@ -757,13 +757,13 @@ ALTER TABLE t_Product_Procedure_Workshop ADD INDEX productNo_workshop (`productN
 
 
 
- ALTER TABLE t_Order_Product add blanking  decimal(4,2)  default 0.0 COMMENT '下料车间 工序数';
-  ALTER TABLE t_Order_Product add frontFm  decimal(4,2)  default 0.0 COMMENT '前道车间 --负面';
-ALTER TABLE t_Order_Product add frontCheck  decimal(4,2)  default 0.0 COMMENT '前道车间 质检';
-ALTER TABLE t_Order_Product add backBigAssist  decimal(4,2)  default 0.0 COMMENT '后到大复工';
+ ALTER TABLE t_Order_Product add blanking  decimal(8,2)  default 0.0 COMMENT '下料车间 工序数';
+  ALTER TABLE t_Order_Product add frontFm  decimal(8,2)  default 0.0 COMMENT '前道车间 --负面';
+ALTER TABLE t_Order_Product add frontCheck  decimal(8,2)  default 0.0 COMMENT '前道车间 质检';
+ALTER TABLE t_Order_Product add backBigAssist  decimal(8,2)  default 0.0 COMMENT '后到大复工';
 
- ALTER TABLE t_Order_Product add backCenterAssist  decimal(4,2)  default 0.0 COMMENT '后道中中复工';
- ALTER TABLE t_Order_Product add assist  decimal(4,2)  default 0.0 COMMENT '外协质检';
- ALTER TABLE t_Order_Product add inspection  decimal(4,2)  default 0.0 COMMENT '成品质检';
+ ALTER TABLE t_Order_Product add backCenterAssist  decimal(8,2)  default 0.0 COMMENT '后道中中复工';
+ ALTER TABLE t_Order_Product add assist  decimal(8,2)  default 0.0 COMMENT '外协质检';
+ ALTER TABLE t_Order_Product add inspection  decimal(8,2)  default 0.0 COMMENT '成品质检';
 
 
